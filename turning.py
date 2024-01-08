65c252cf498fe07864ced51bd30a22fa470b173a
JaydenProgram <jaydenblom2005@gmail.com>
2024-01-08 09:14:09 +0100

    turning.py working

--- turning.py
+++ turning.py
@@ -6,26 +6,22 @@ import time
 
 pygame.init()
 pygame.mixer.init()
-# Initialize the PlayStation controller
+
 pygame.joystick.init()
 
+# Check the number of connected joysticks
+num_joysticks = pygame.joystick.get_count()
+print("Number of connected joysticks:", num_joysticks)
 
-while True:
-    connected_controllers = pygame.joystick.get_count()
-    if connected_controllers >= 2:
-        print("Two controllers detected. Continuing with the program...")
-        break
-    else:
-        print("Waiting for at least two controllers to be connected...")
-        pygame.time.delay(1000)
+# Initialize each joystick
+for i in range(num_joysticks):
+    joystick = pygame.joystick.Joystick(i)
+    joystick.init()
+    print("Joystick {} name:".format(i), joystick.get_name())
 
-# Initialize the first joystick
+# Example: Assigning each joystick to a player
 joystick = pygame.joystick.Joystick(0)
 joystick1 = pygame.joystick.Joystick(1)
-joystick.init()
-joystick1.init()
-
-# Define controller state for each button for all connected joysticks
 
 
 print(f"Initialized {joystick.get_name()} and {joystick1.get_name()}")
@@ -59,6 +55,7 @@ for filename in os.listdir(folder_path):
         }
 # Define controller state for each button
 controller_states = {i: False for i in range(joystick.get_numbuttons())}
+controller_states_1 = {i: False for i in range(joystick1.get_numbuttons())}
 
 # State to track if we are waiting for a button press
 waiting_for_button = False
@@ -72,7 +69,10 @@ try:
     while True:
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONUP:
-                print(f"Button {event.button} pressed")
+                if event.joy == 0:
+                   print(f"Button {event.button} pressed on {joystick.get_name()}")
+                else:
+                   print (f"Button {event.button} pressed on {joystick1.get_name()}")
 
                 if not waiting_for_button:
                     # Check if the PS button on the controller is pressed
@@ -82,22 +82,15 @@ try:
                         waiting_for_button = True
                         if event.button in button_sounds:
                             sound_name = button_sounds[event.button]
-                if not waiting_for_button:
-                    # Check if the PS button on the controller is pressed
-                    if event.button == 8:  # PS button on PlayStation controller
-                        print("Press another button to toggle or hold for test mode")
-                        sounds['testmodus_ingeschakeld'].play()
-                        waiting_for_button = True
-                        test_mode_start_time = current_time
-                        if event.button in button_sounds:
-                            sound_name = button_sounds[event.button]
-                            
                 else:
                     # Toggle the state of the pressed button
                     if event.button != 10:
-                        controller_states[event.button] = not controller_states[event.button]
-                        print(f"Button {event.button} toggled")
-                        print(f"Button {event.button} toggled to {controller_states[event.button]}")
+                        if event.joy == 0:
+                           controller_states[event.button] = not controller_states[event.button]
+                           print(f"Button {event.button} toggled to {controller_states[event.button]}")
+                        else:
+                           controller_states_1[event.button] = not controller_states_1[event.button]
+                           print(f"Button {event.button} toggled to {controller_states_1[event.button]}")
                         waiting_for_button = False
                         if event.button in button_sounds:
                             sound_name = button_sounds[event.button]
@@ -118,25 +111,11 @@ try:
                                     sounds['ingeschakeld'].play()  # Last sound
                                     pygame.time.wait(int(sounds['ingeschakeld'].get_length() * 1000))
 
-            elif event.type == pygame.JOYBUTTONUP and not controller_states[event.button] and not waiting_for_button:
-                # Handle button release only if it's not toggled
-                print(f"Button {event.button} released")
+                    elif event.type == pygame.JOYBUTTONUP and not controller_states[event.button] and not waiting_for_button:
+                       # Handle button release only if it's not toggled
+                       print(f"Button {event.button} released")
                 
-        # Check for test mode activation
-        if waiting_for_button and current_time - test_mode_start_time > 2.0:
-            waiting_for_button = False
-            print("Entering test mode. Hold the PS button to exit.")
-            sounds['testmodus_ingeschakeld'].play()
-
-            while joystick.get_button(10):  # Continue test mode as long as PS button is held
-                for event in pygame.event.get():
-                    if event.type == pygame.JOYBUTTONDOWN and event.button != 10:
-                        print(f"Test mode: Button {event.button} is {'on' if controller_states[event.button] else 'off'}")
-
-            print("Exiting test mode.")
-            sounds['testmodus_uitgeschakeld'].play()
-    
-
+        
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
